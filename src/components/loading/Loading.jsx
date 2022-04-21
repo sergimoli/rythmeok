8b38ef6134bb746500fb7193a095c0ce2b30037a
jQta <estevezpereira@gmail.com>
2022-04-21 21:08:06 +0200

    FEAT: component loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,19 @@
+import './Loading.scss';
+
+function Loading() {
+
+    return (
+        <div class="loader">
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+            <span class="stroke"></span>
+
+        </div>
+    );
+}
+
+export default Loading;
