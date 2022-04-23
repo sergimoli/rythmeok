87ad100942d7a566e3d4070e737df39dbbb24052
jQta <estevezpereira@gmail.com>
2022-04-23 23:07:36 +0200

    FEAT: some changes in configuration page

--- src/pages/configuration/ConfigurationPage.jsx
+++ src/pages/configuration/ConfigurationPage.jsx
@@ -3,59 +3,60 @@ import "./ConfigurationPage.scss"
 
 export default function ConfigurationPage() {
   return (
-    <div className="b-section">
-      <>
-        <h3 className="b-title__h3">PERSONAL</h3>
-        <button className="b-btn">
-          Editar perfil
-        </button>
-        <button className="b-btn">
-          Ubicación
-        </button>
-        <button className="b-btn">
-          Sincronizar calendario
-        </button>
-        <button className="b-btn">
-          Notificaciones
-        </button>
-        <button className="b-btn">
-          Medios de pago
-        </button>
-        <button className="b-btn">
-          Historial de compras
-        </button>
-      </>
-      <div className="b-section">
-        <h3 className="b-title__h3">SINCRONIZAR PREFERENCIAS</h3>
-        <button className="b-btn">
-          Sincroniza tu música
-        </button>
-        <button className="b-btn">
-          Sincroniza tus salas favoritas
-        </button>
-        <button className="b-btn">
-          Sincroniza tus radios y podcast
-        </button>
-        <button className="b-btn">
-          Sincroniza tus revistas
-        </button>
-        <button className="b-btn">
-          Sincroniza tus contactos
-        </button>
-      </div>
-      <></>
-      <div className="b-section">
-      <br></br>
-        <button className="b-btn">
-          Ayuda
-        </button>
-        <button className="b-btn">
-          Cerrar sesión
-        </button>
-      </div>
-      <div>
+    <>
+      <header className="head-bar">
+        <p className="head-bar__p">Configuration</p>
+      </header>
+      <section className="config-container">
+        <div className="b-section">
+          <h3 className="b-section__title">PERSONAL</h3>
+          <button className="b-section__btn">
+            Editar perfil
+          </button>
+          <button className="b-section__btn">
+            Ubicación
+          </button>
+          <button className="b-section__btn">
+            Sincronizar calendario
+          </button>
+          <button className="b-section__btn">
+            Notificaciones
+          </button>
+          <button className="b-section__btn">
+            Medios de pago
+          </button>
+          <button className="b-section__btn">
+            Historial de compras
+          </button>
+        </div>
+        <div className="b-section">
+          <h3 className="b-section__title">SINCRONIZAR PREFERENCIAS</h3>
+          <button className="b-section__btn">
+            Sincroniza tu música
+          </button>
+          <button className="b-section__btn">
+            Sincroniza tus salas favoritas
+          </button>
+          <button className="b-section__btn">
+            Sincroniza tus radios y podcast
+          </button>
+          <button className="b-section__btn">
+            Sincroniza tus revistas
+          </button>
+          <button className="b-section__btn">
+            Sincroniza tus contactos
+          </button>
+        </div>
+        <div className="b-section b-section--bottom">
+          <button className="b-section__btn">
+            Ayuda
+          </button>
+          <button className="b-section__btn">
+            Cerrar sesión
+          </button>
+        </div>
         <Navbar />
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
