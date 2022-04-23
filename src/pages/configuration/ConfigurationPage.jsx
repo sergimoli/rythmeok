04b3a2afbbbebc16c011a1a3748269e6eafb79d2
SaraRL23 <saraanna03@hotmail.com>
2022-04-23 21:53:59 +0200

    configurationpage

--- src/pages/configuration/ConfigurationPage.jsx
+++ src/pages/configuration/ConfigurationPage.jsx
@@ -3,51 +3,53 @@ import "./ConfigurationPage.scss"
 
 export default function ConfigurationPage() {
   return (
-    <div>
+    <div className="b-section">
       <>
-        <h3>PERSONAL</h3>
-        <button className="">
+        <h3 className="b-title__h3">PERSONAL</h3>
+        <button className="b-btn">
           Editar perfil
         </button>
-        <button className="">
+        <button className="b-btn">
           Ubicación
         </button>
-        <button className="">
+        <button className="b-btn">
           Sincronizar calendario
         </button>
-        <button className="">
+        <button className="b-btn">
           Notificaciones
         </button>
-        <button className="">
+        <button className="b-btn">
           Medios de pago
         </button>
-        <button className="">
+        <button className="b-btn">
           Historial de compras
         </button>
       </>
-      <div>
-        <h3>SINCRONIZAR PREFERENCIAS</h3>
-        <button className="">
+      <div className="b-section">
+        <h3 className="b-title__h3">SINCRONIZAR PREFERENCIAS</h3>
+        <button className="b-btn">
           Sincroniza tu música
         </button>
-        <button className="">
+        <button className="b-btn">
           Sincroniza tus salas favoritas
         </button>
-        <button className="">
+        <button className="b-btn">
           Sincroniza tus radios y podcast
         </button>
-        <button className="">
+        <button className="b-btn">
           Sincroniza tus revistas
         </button>
-        <button className="">
+        <button className="b-btn">
           Sincroniza tus contactos
         </button>
       </div>
-      <div>
-        <button className="">
+      <></>
+      <div className="b-section">
+      <br></br>
+        <button className="b-btn">
           Ayuda
         </button>
-        <button className="">
+        <button className="b-btn">
           Cerrar sesión
         </button>
       </div>
