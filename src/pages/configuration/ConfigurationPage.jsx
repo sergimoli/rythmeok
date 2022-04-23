581fe979035812e1d1ed3e32a3ab7ad828c4dc9a
SaraRL23 <saraanna03@hotmail.com>
2022-04-23 15:48:00 +0200

    buttonsconfig

--- src/pages/configuration/ConfigurationPage.jsx
+++ src/pages/configuration/ConfigurationPage.jsx
@@ -1,7 +1,59 @@
+import Navbar from "../../components/navbar/Navbar";
 import "./ConfigurationPage.scss"
 
 export default function ConfigurationPage() {
   return (
-    <div>ConfigurationPage</div>
-  )
+    <div>
+      <>
+        <h3>PERSONAL</h3>
+        <button className="">
+          Editar perfil
+        </button>
+        <button className="">
+          Ubicación
+        </button>
+        <button className="">
+          Sincronizar calendario
+        </button>
+        <button className="">
+          Notificaciones
+        </button>
+        <button className="">
+          Medios de pago
+        </button>
+        <button className="">
+          Historial de compras
+        </button>
+      </>
+      <div>
+        <h3>SINCRONIZAR PREFERENCIAS</h3>
+        <button className="">
+          Sincroniza tu música
+        </button>
+        <button className="">
+          Sincroniza tus salas favoritas
+        </button>
+        <button className="">
+          Sincroniza tus radios y podcast
+        </button>
+        <button className="">
+          Sincroniza tus revistas
+        </button>
+        <button className="">
+          Sincroniza tus contactos
+        </button>
+      </div>
+      <div>
+        <button className="">
+          Ayuda
+        </button>
+        <button className="">
+          Cerrar sesión
+        </button>
+      </div>
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
 }
