8d7821b534a1cf6020187e893aab09215104b98e
jQta <estevezpereira@gmail.com>
2022-04-30 21:35:39 +0200

    FEAT: Register/login app

--- src/pages/fanClubPage/FanClubPage.jsx
+++ src/pages/fanClubPage/FanClubPage.jsx
@@ -6,31 +6,31 @@ import { useState } from "react";
 import Bienvenido from "../../shared/components/FanClub/Bienvenido";
 
 export default function FanClubPage() {
-  const [register, setRegister]= useState(false)
+  const [ register, setRegister ] = useState(false)
 
   return (
     <>
-      {register ? <Register/> : (
-     <>
-      <header className="head-bar">
-        <p className="head-bar__p">Fan Club</p>
-      </header>
-    <div className="config-navbar">
-      <div className="b-main">
-    <img className="b-main__logo" src={logoRithm} alt="logo Fan Club" />
-    <h3 className="b-main__logo__h3">Bienvenido al club donde vivirás tu música.</h3>
-    <p className="b-main__logo__p">RYTHME te ofrecerá aquí todo un mundo de experiencias exclusivas para apasionados de la musica tan exigentes como tú</p>
-    <p className="b-main__logo__p">¿Te unes?</p>
-    <button className="b-btn__logo" onClick={()=>setRegister(true)}>Quiero unirme ahora</button>
-    <a className='b-link_register' href='/home'>Más tarde</a>
-    </div>
-    </div>
-   
-    <Navbar/>
-    <Bienvenido/>
-    </>
-    )}
-   
+      {register ? <Register /> : (
+        <>
+          <header className="head-bar">
+            <p className="head-bar__p">Fan Club</p>
+          </header>
+          <div className="config-navbar">
+            <div className="b-main">
+              <img className="b-main__logo" src={logoRithm} alt="logo Fan Club" />
+              <h3 className="b-main__logo__h3">Bienvenido al club donde vivirás tu música.</h3>
+              <p className="b-main__logo__p">RYTHME te ofrecerá aquí todo un mundo de experiencias exclusivas para apasionados de la musica tan exigentes como tú</p>
+              <p className="b-main__logo__p">¿Te unes?</p>
+              <button className="b-btn__logo" onClick={() => setRegister(true)}>Quiero unirme ahora</button>
+              <a className='b-link_register' href='/home'>Más tarde</a>
+            </div>
+          </div>
+
+          <Navbar />
+          <Bienvenido />
+        </>
+      )}
+
     </>
   )
 }
--- src/shared/components/FanClub/Bienvenido.jsx
+++ src/shared/components/FanClub/Bienvenido.jsx
@@ -8,24 +8,24 @@ import iconGroup from '../../../assets/images/group_add_FILL0_wght400_GRAD0_opsz
 
 export default function Bienvenido() {
   return (
-      <>
+    <>
       <header className="b-head-bar">Fan Club</header>
-    <h1 className='b-line'>¡Bienvenido!</h1>
-    <div className='b-circle'>
-        <a href='#' ><img className='b-img1' src={iconUser} alt='usuario'/></a>
-        <a href='#' ><img className='b-img2' src={iconZero} alt='0'/></a>
-        <a href='#' ><img className='b-img3' src={iconChat} alt='chat'/></a>
-        <a href='#' ><img className='b-img4' src={iconZero} alt='0'/></a>
-        <a href='#' ><img className='b-img5' src={iconGroup} alt='grupo'/></a>
-    </div>
-    <ul className='b-list'>
-       <li><a href='/#'>Perfil</a></li>
+      <h1 className='b-line'>¡Bienvenido!</h1>
+      <div className='b-circle'>
+        <a href='#' ><img className='b-img1' src={iconUser} alt='usuario' /></a>
+        <a href='#' ><img className='b-img2' src={iconZero} alt='0' /></a>
+        <a href='#' ><img className='b-img3' src={iconChat} alt='chat' /></a>
+        <a href='#' ><img className='b-img4' src={iconZero} alt='0' /></a>
+        <a href='#' ><img className='b-img5' src={iconGroup} alt='grupo' /></a>
+      </div>
+      <ul className='b-list'>
+        <li><a href='/#'>Perfil</a></li>
         <li><a href='/#'>Fan Store</a></li>
         <li><a href='/#'>Tours & Tickets</a></li>
         <li><a href='/#'>Fan Wall</a></li>
         <li><a href='/#'>Amigos y mensajes</a></li>
-    </ul>
-    <Navbar/>
+      </ul>
+      <Navbar />
     </>
   )
 }
