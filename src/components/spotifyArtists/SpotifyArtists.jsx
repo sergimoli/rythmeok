46ccd4e3dbd2a53343ba67b773a1507a090e5044
Sergi Molina <sergimoli@gmail.com>
2022-05-03 16:27:44 +0200

    changes in spoty and map

--- src/components/spotifyArtists/SpotifyArtists.jsx
+++ src/components/spotifyArtists/SpotifyArtists.jsx
@@ -2,11 +2,12 @@ import React, { useContext } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Tokenspotify } from "../../shared/contexts/Tokenspotify";
-import "./SpotifyArtists.scss"
-import imageArtist from "../../assets/images/imageArtist.jpg"
-import checkIcon from "../../assets/icons/checkGreen.png"
+import "./SpotifyArtists.scss";
+import imageArtist from "../../assets/images/imageArtist.jpg";
+import checkIcon from "../../assets/icons/checkGreen.png";
+const baseURL = process.env.REACT_APP_BACK_URL;
 
-export default function SpotifyArtists() {
+export default function SpotifyArtists({ artista }) {
   //const CLIENT_ID = "5c612d5225894ac99b4a7f9e5316aea3"; //original Sergi
   const CLIENT_ID = "78f40f4bcaab4b3e8ca6990708d8ad37"; // from Sara!
   const REDIRECT_URI = "http://localhost:3000/configuration";
@@ -15,7 +16,8 @@ export default function SpotifyArtists() {
 
   const [token, setToken] = useState("");
   // const [searchKey, setSearchKey] = useState("");
-  const [searchKey, setSearchKey] = useState("Ed Sheeran");
+  // const [searchKey, setSearchKey] = useState("Ed Sheeran");
+  const [searchKey, setSearchKey] = useState(artista);
   const [artists, setArtists] = useState([]);
   const [profile, setProfile] = useState(null);
   const [profileName, setProfileName] = useState(null);
@@ -28,6 +30,9 @@ export default function SpotifyArtists() {
     //let token2 = window.localStorage.getItem("token");
     let token = spoty;
     console.log("token:", token);
+
+    console.log("artist:", { artista });
+    console.log("artist2:", artista);
 
     if (!token && hash) {
       token = hash
@@ -53,13 +58,15 @@ export default function SpotifyArtists() {
       axios.defaults.headers["Content-Type"] = "application/json";
       const getCurrentUserProfile = () => axios.get("/me");
 
+      console.log("artista found:", artista);
+
       const fetchData = async () => {
         try {
           const { data } = await getCurrentUserProfile();
           setProfile(data);
           console.log("the profile is:", data.display_name);
           setProfileName(data.display_name);
-          setSearchKey("Ed Sheeran");
+          ///////////////setSearchKey("Ed Sheeran");
           console.log("the text to look for iss:", searchKey);
           // setShowData(true);
           searchArtists();
@@ -98,24 +105,41 @@ export default function SpotifyArtists() {
     console.log("artist info:", artists);
     return (
       <div className="b-containartist">
-    { artists.map((artist) => (
-      <div className="b-boxartist" key={artist.id}>
-        {artist.images.length ? (
-          <img className="b-boxartist__img" width={"100%"} src={artist.images[0].url} alt="" />
-        ) : (
-          <img className="b-boxartist__img" src={imageArtist} alt="imageArtist" />
-        )}
-        <div className="b-boxartist__cont">
-        <div className="b-boxartist__infoArtist">
-        <h2 className="b-boxartist__name">{artist.name}</h2>
-        <p className="b-boxartist__followers">{artist.followers.total} segidores</p>
-        </div>
-        </div>
-        <div className="b-boxartist__checkdiv">
-          <img className="b-boxartist__checkicon"src={checkIcon} alt="checkIcon" />
-        </div>
+        {artists.map((artist) => (
+          <div className="b-boxartist" key={artist.id}>
+            {artist.images.length ? (
+              <img
+                className="b-boxartist__img"
+                width={"100%"}
+                src={artist.images[0].url}
+                alt=""
+              />
+            ) : (
+              <img
+                className="b-boxartist__img"
+                src={imageArtist}
+                alt="imageArtist"
+              />
+            )}
+            <div className="b-boxartist__cont">
+              <div className="b-boxartist__infoArtist">
+                <h2 className="b-boxartist__name">{artist.name}</h2>
+                <p className="b-boxartist__followers">
+                  {artist.followers.total} seguidores
+                </p>
+              </div>
+            </div>
+            <div className="b-boxartist__checkdiv">
+              <img
+                className="b-boxartist__checkicon"
+                src={checkIcon}
+                alt="checkIcon"
+              />
+            </div>
+          </div>
+        ))}
       </div>
-    ))}</div>);
+    );
   };
 
   return (
@@ -135,11 +159,13 @@ export default function SpotifyArtists() {
         ) : (
           <>
             <h3 className="b-spotyartist__name">¡Hola {profileName}!</h3>
-            <p  className="b-spotyartist__text">
-              proximamente irás al concierto de {searchKey}. ¿Quieres
+            <p className="b-spotyartist__text">
+              Próximamente irás al concierto de {searchKey}. ¿Quieres
               sincronizar con artistas similares?
             </p>
-            <button className="b-logout__btn" onClick={logout}>Logout</button>
+            <button className="b-logout__btn" onClick={logout}>
+              Logout
+            </button>
             {showData && renderArtists()}
           </>
         )}
--- src/components/synchroProcess1/SynchroProcess1.jsx
+++ src/components/synchroProcess1/SynchroProcess1.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import SynchroProcess2 from "../synchroProcess2/SynchroProcess2";
 import "./SynchroProcess1.scss";
+import axios from "axios";
+const baseURL = process.env.REACT_APP_BACK_URL;
 
 export default function SynchroProcess1() {
   const [syncro, setSyncro] = useState(false);
+  const userLocal = JSON.parse(localStorage.getItem("user"));
+  const [artist, setArtist] = useState(null);
+
+  const onSyncro = () => {
+    if (userLocal) {
+      const getUser = async () => {
+        if (userLocal) {
+          const res = await axios.get(baseURL + `/users/${userLocal._id}`);
+          setArtist(res.data.purchases[0].artist[0].name);
+        }
+      };
+      getUser();
+    }
+    setSyncro(true);
+  };
+
   return (
     <>
       {syncro ? (
-        <SynchroProcess2></SynchroProcess2>
+        <SynchroProcess2 artista={artist}></SynchroProcess2>
       ) : (
         <>
           <div className="b-main">
@@ -21,7 +39,7 @@ export default function SynchroProcess1() {
             <h2 className="b-main__logo__h2">
               ¿Quieres sincronizar tus preferencias musicales con Spotify?
             </h2>
-            <button className="b-btn__logo" onClick={() => setSyncro(true)}>
+            <button className="b-btn__logo" onClick={() => onSyncro()}>
               Sincronizar con Spotify
             </button>
             <a className="b-link_register" href="/home">
